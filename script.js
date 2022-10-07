dd2ad938c043a33cf6ae0b29f557c2051fd87d9e
gnahc2305 <achangmurillo@gmail.com>
2022-10-07 00:19:24 -0500

    make div buttons and assign them a listener

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const sections_btn = Array.from(document.querySelectorAll('.section'));
+
 const Gameboard = (function () {
     let gameBoard = [0, 1, 2, 
                      3, 4, 5,
                      6, 7, 8];
-    console.log(gameBoard);
+    
 })();
+
+sections_btn.map(button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id);
+    })
+})
 
